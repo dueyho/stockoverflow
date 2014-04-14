fddd1c05ba565e5cc5ee94834cb18452d6d2c305
Due Ho & Harland Hauger <student@epicodus.com>
2014-04-14 09:15:31 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.0.4'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'capybara'
+gem 'bcrypt'
+gem 'paperclip'
+gem 'smarter_csv'
+
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'factory_girl'
+  gem 'launchy'
+  gem 'faker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stackoverflow::Application.config.secret_key_base = 'c23c97771c11f631d80745039c95a7f7b4a4c0ae4786331277394029a7f898f57dd5d50a8073983080b99f689bb2dee4315db707378f954a972e057104e6d575'
